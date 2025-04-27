23a6ec50e9e17f0a6e5d26afaa8c570287690389
Bayram Enes Atay <bayrameness4242@gmail.com>
2025-04-27 18:28:26 +0300

    Add files via upload

---
+++ odev1.kt
@@ -0,0 +1,33 @@
+package com.bayram.odev1yedek
+
+
+
+fun main()
+{
+    val sehir="Ankara"
+    val ulke="Turkiye"
+    val mail="bayram"
+    val maas=500000
+    val yemekfiyati=455
+    val bakiye=45
+    val agirlik=55.4
+
+    val medeniDurum = "Bekar"
+    val urunYorum = "Çok iyi bir ürün."
+    val odemeTarihi = "2025-04-01"
+    val odeme = 320.50
+    val siparisAdeti = 3
+    val arabaModeli = "Toyota Corolla"
+    val kitapAdi = "Kotlin Programlama"
+    var renk="mavi"
+
+    val indirimmiktari=0.45
+
+    println("Sehir: $sehir")
+    println("Ulke: $ulke")
+    println("Mail: $mail")
+    println(yemekfiyati)
+    println(bakiye)
+    println("agirlik: $agirlik")
+
+}
